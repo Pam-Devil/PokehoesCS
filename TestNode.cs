a7e20bb257d21064865a849fbba54fe2490708cf
Pam-Devil <jonatascardoso0573@gmail.com>
2022-11-09 12:15:08 -0300

    Creating my repo

---
+++ TestNode.cs
@@ -0,0 +1,69 @@
+using Godot;
+using System;
+
+
+
+public class TestNode : Node
+{
+    //
+
+    public override void _Ready() {
+        
+        Foo,
+        bar,
+        bazy,
+        buzz
+
+    }
+    
+        
+        Foo,
+        bar,
+        bazy,
+        buzz
+
+            Foo,
+            bar,
+            bazy,
+            ident,
+            buzz
+
+        
+        Foo,
+        bar,
+        bazy,
+        
+        Foo,
+        bar,
+        bazy,
+        buzz
+        buzz
+        
+        
+        
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
+}
+
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
+}
+
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
+}
+
+
+
+
